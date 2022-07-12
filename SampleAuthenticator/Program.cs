ee0ef7bbd059c6caaf80aa69b2e0237661ba8d06
MSI-Z370-A-PRO\RahulMore <rahul.more7861@gmail.com>
2022-07-12 15:03:21 +0530

    Add project files.

---
+++ SampleAuthenticator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SampleAuthenticator // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key;//= args[0];
+            //key = "JYD3E2HR5K4P5J5L23U5JXITPM6RJ2AYRP32ZGT7GASFVNET5J7Q"; //AMAZON
+            key = "ia2eioedjfposo3l"; //Microsoft
+
+            var bytes = Base32Encoding.ToBytes(key);
+
+            var totp = new Totp(bytes);
+
+            var result = totp.ComputeTotp(8);
+            var remainingTime = totp.RemainingSeconds();
+
+            Console.WriteLine(result);
+            Console.WriteLine(remainingTime);
+            Console.ReadLine();
+        }
+    }
+}
